4790a028fdb0e40c4664dd4381dd57d9af7fee0e
Igor Nardoni <igornardonik@gmail.com>
2023-09-20 18:46:09 -0300

    add content

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from gallery.views import index, imagem
+from gallery.views import index, imagem, buscar
 
 urlpatterns = [
     path('', index, name='index'),
-    path('imagem/<int:foto_id>', imagem, name='imagem')
+    path('imagem/<int:foto_id>', imagem, name='imagem'),
+    path('buscar', buscar, name='buscar')
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -10,3 +10,11 @@ def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'gallery/imagem.html', {"fotografia": fotografia})
 
+def buscar(request):
+    fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET["buscar"]
+        if nome_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+    return render(request, 'gallery/buscar.html', {"cards": fotografias})
+
